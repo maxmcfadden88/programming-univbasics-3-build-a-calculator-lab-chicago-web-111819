a9d5c4e5acb235d56d90d48c05889f7d074d53a6
Max McFadden <maxmcfadden88@gmail.com>
2019-11-08 22:28:35 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,41 @@
 def addition(num1, num2)
-
+addition = num1 + num2
+p addition
 end
+
+addition(5, 4)
 
 def subtraction(num1, num2)
-
+subtraction = num1 - num2
+p subtraction
 end
+
+subtraction(10, 5)
 
 def division(num1, num2)
-
+division = num1 / num2
+p division
 end
+
+division(50, 2)
 
 def multiplication(num1, num2)
-
+multiplication = num1 * num2
+p multiplication
 end
+
+multiplication(4, 30)
 
 def modulo(num1, num2)
-
+modulo = (num1).remainder(num2)
+p modulo
 end
+
+modulo(34, 5)
 
 def square_root(num)
-
+square_root = Math.sqrt(num)
+p square_root
 end
+
+square_root(81)
