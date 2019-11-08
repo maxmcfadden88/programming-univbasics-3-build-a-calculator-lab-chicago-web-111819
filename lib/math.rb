1616d20f6094680894e6990db2cca9261e09d621
Max McFadden <maxmcfadden88@gmail.com>
2019-11-08 22:13:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+num1 + num2
+p addition
 end
 
 def subtraction(num1, num2)
-
+num1 - num2
+p subtraction
 end
 
 def division(num1, num2)
-
+num1 / num2
+p division
 end
 
 def multiplication(num1, num2)
-
+num2 * num2
+p multiplication
 end
 
 def modulo(num1, num2)
